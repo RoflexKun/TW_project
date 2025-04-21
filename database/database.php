d6c8c29fb941b1970c8cbf0e952139df843f34c7
RoflexKun <razvan.vilcu2004@gmail.com>
2025-04-21 22:05:52 +0300

    hide credentials to the DB

--- database/database.php
+++ database/database.php
@@ -1,7 +1,15 @@
 <?php
-$db_username = 'pets2adopt';
-$db_password = 'parola123';
-$db_connection = 'localhost/XE';
+$env = parse_ini_file(__DIR__.'/../misc/.env');
+
+$db_username = $env['DB_USERNAME'];
+$db_password = $env['DB_PASSWORD'];
+$db_connection = $env['DB_CONNECTION'];
 
 $conn = oci_connect($db_username, $db_password, $db_connection);
+
+if(!$conn)
+{
+    $e = oci_error();
+    die("Connection failed: " . $e['message']);
+}
 ?>
