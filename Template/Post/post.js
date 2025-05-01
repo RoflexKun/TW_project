d103bf75bebcff32fe894803932ffa5a602baef1
RoflexKun <razvan.vilcu2004@gmail.com>
2025-05-01 20:01:44 +0300

    Written the PL/SQL Script for extracting data about a post. Not working backend-frontend connection

--- Template/Post/post.js
+++ Template/Post/post.js
@@ -1,10 +1,59 @@
-const URLparams = new URLSearchParams(window.location.search);
-const postId = URLparams.get('id');
+async function extractPostInfo(){
+    const URLparams = new URLSearchParams(window.location.search);
+    const postId = URLparams.get('id');
+    
+    const formData = new FormData();
+    formData.append('id', postId);
+    
+    try{
+        var response = await fetch('http://localhost/database/extractPostInfo.php', {
+            method: 'POST',
+            body: formData
+        });
+    
+        var result = await response.text();
+        console.log(result);
 
-const formData = new FormData();
-formData.append(postId)
+        if(result.error){
+            console.log(result.error);
+            return;
+        }
+        else{
+            document.getElementById('pet-name').textContent = result.name;
+            document.getElementById('pet-species').textContent = result.species;
+            document.getElementById('pet-breed').textContent = result.breed;
+            document.getElementById('pet-birthday').textContent = result.birthday;
+            document.getElementById('pet-age').textContent = result.age;
+            document.getElementById('pet-location').textContent = result.location;
 
-fetch('extractPostInfo.php', {
-    method: 'POST',
-    body: formData
-})
+            const mediaSection = document.querySelector('.photo-section');
+            const mediaArray = result.media_array.split(';');
+
+            const photoExt = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+            const videoExt = ['mp4', 'webm'];
+
+            mediaArray.forEach(file_path => {
+                const extension = file_path.split('.').pop().toLowerCase();
+                if(photoExt.includes(extension)){
+                    const img = document.createElement('img');
+                    img.src = `${file_path}`;
+                    mediaSection.appendChild(img);
+                }
+                else if(videoExt.includes(extension)){
+                    const video = document.createElement('video');
+                    video.src = `${file_path}`;
+                    mediaSection.appendChild(video);
+                }
+            })
+
+        }
+ 
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+extractPostInfo();
+
